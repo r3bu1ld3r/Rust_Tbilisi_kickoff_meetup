4d15999ad3c35ebc0110a996ed7175c93514906e
Anton Velikanov <61997468+r3bu1ld3r@users.noreply.github.com>
2025-02-16 15:46:56 +0400

    better description

--- src/lib.rs
+++ src/lib.rs
@@ -10,14 +10,6 @@ use std::{
     thread,
 };
 
-// Five philosophers dine together at the same table.
-// Each philosopher has their own place at the table.
-// There is a fork between each plate. The dish served is a kind of spaghetti which has to be eaten with two forks.
-// Each philosopher can only alternately think and eat.
-// Moreover, a philosopher can only eat their spaghetti when they have both a left and right fork.
-// Thus two forks will only be available when their two nearest neighbors are thinking, not eating.
-// After an individual philosopher finishes eating, they will put down both forks.
-
 struct Fork;
 
 struct Philosopher {
