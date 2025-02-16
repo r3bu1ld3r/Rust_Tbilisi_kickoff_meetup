8624f4279d76e8a746f2aa0fb571044f474ff5aa
Anton Velikanov <61997468+r3bu1ld3r@users.noreply.github.com>
2025-02-16 15:02:47 +0400

    initial

---
+++ README.md
@@ -0,0 +1,41 @@
+# Test concurrent code in Rust with Loom
+
+## Description of the Problem
+
+This is the classic Dining Philosophers problem:
+Five philosophers dine together at the same table. Each philosopher has their own place at the table, and there is a fork between each plate. The dish served is a kind of spaghetti that must be eaten with two forks.
+Each philosopher can only alternate between thinking and eating. Moreover, a philosopher can only eat their spaghetti when they have both a left and a right fork. Thus, two forks will only be available when their two nearest neighbors are thinking rather than eating. After an individual philosopher finishes eating, they put down both forks.
+
+The challenge of this problem arises from the need to manage concurrent access to the limited resources (the forks) while avoiding deadlock (where no progress is made because all philosophers are waiting indefinitely to acquire forks) and starvation (where a philosopher never gets to eat because others always take precedence).
+
+## Description of the Solution
+
+This implementation of the Dining Philosophers problem provides two solutions:
+
+1. **Naive Solution**:
+   - In the naive solution, each philosopher tries to pick up their left fork and then their right fork in sequence.
+   - This approach can lead to a deadlock situation where each philosopher holds one fork and waits indefinitely for the other fork to become available.
+
+2. **Deadlock-Free Solution**:
+   - The deadlock-free solution introduces a break in symmetry. Specifically, one philosopher (the last in the seating arrangement) switches the order in which they pick up the forks. Instead of picking up the left fork first, then the right fork, they take the right fork first and then the left fork.
+   - By doing this, the circular wait condition necessary for deadlock is prevented, allowing the program to run indefinitely without freezing.
+
+Both solutions are tested using [`loom`], a tool in Rust to check for concurrency issues. `loom` allows you to test all possible interleavings of threads, ensuring the robustness of concurrent algorithms.
+
+## How to Run the Tests with Loom Enabled
+
+To run the tests using `loom`, ensuring thread safety and the absence of deadlocks:
+
+**Run Tests**:
+You can run the tests with `loom` by enabling the `loom` configuration:
+ - For triggering a bug:
+```bash
+LOOM_MAX_PREEMPTIONS=2 RUSTFLAGS="--cfg loom" cargo test -r -- --exact trigger_deadlock_with_loom --nocapture
+```
+- For running without a bug:
+```bash
+LOOM_MAX_PREEMPTIONS=2 RUSTFLAGS="--cfg loom" cargo test -r -- --exact run_correct_solution_with_loom --nocapture
+```
+
+When running with `loom`, the code is tested with reduced iterations (10 instead of 100) because `loom` performs an exhaustive exploration of all possible thread interleavings. This reduction is necessary since the exhaustive checking provided by `loom` can become computationally expensive with higher iteration counts.
+Additionally, the number of philosophers is reduced to 3 instead of 5 to further simplify the concurrency scenario, thereby limiting the state space and ensuring that `loom` can effectively analyze all interleavings. LOOM_MAX_PREEMPTIONS=2 is also used to cap the number of preemptions, which controls the number of interleavings and makes the thorough testing by `loom` practical and efficient.
---
+++ src/lib.rs
@@ -0,0 +1,163 @@
+#[cfg(loom)]
+use loom::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+#[cfg(not(loom))]
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+// Five philosophers dine together at the same table.
+// Each philosopher has their own place at the table.
+// There is a fork between each plate. The dish served is a kind of spaghetti which has to be eaten with two forks.
+// Each philosopher can only alternately think and eat.
+// Moreover, a philosopher can only eat their spaghetti when they have both a left and right fork.
+// Thus two forks will only be available when their two nearest neighbors are thinking, not eating.
+// After an individual philosopher finishes eating, they will put down both forks.
+
+struct Fork;
+
+struct Philosopher {
+    name: String,
+    left_fork: Arc<Mutex<Fork>>,
+    right_fork: Arc<Mutex<Fork>>,
+    thoughts: mpsc::Sender<String>,
+}
+
+impl Philosopher {
+    pub fn new(
+        name: String,
+        left_fork: Arc<Mutex<Fork>>,
+        right_fork: Arc<Mutex<Fork>>,
+        thoughts: mpsc::Sender<String>,
+    ) -> Self {
+        Self {
+            name,
+            left_fork,
+            right_fork,
+            thoughts,
+        }
+    }
+
+    fn think(&self) {
+        self.thoughts
+            .send(format!("Eureka! {} has a new idea!", &self.name))
+            .unwrap();
+    }
+
+    fn eat(&self) {
+        match (self.left_fork.lock(), self.right_fork.lock()) {
+            (Ok(_l), Ok(_r)) => {
+                println!("{} is eating...", &self.name);
+            }
+            (_, _) => {
+                println!("{} couldn't get both forks", &self.name);
+            }
+        }
+    }
+}
+
+#[cfg(loom)]
+static PHILOSOPHERS: &[&str] = &["Socrates", "Hypatia", "Plato"];
+#[cfg(not(loom))]
+static PHILOSOPHERS: &[&str] = &["Socrates", "Hypatia", "Plato", "Aristotle", "Pythagoras"];
+
+pub fn solution(correct: bool) {
+    let forks = (0..PHILOSOPHERS.len())
+        .map(|_| Arc::new(Mutex::new(Fork)))
+        .collect::<Vec<_>>();
+
+    let (tx, rx) = mpsc::channel();
+
+    let philosophers = PHILOSOPHERS
+        .iter()
+        .enumerate()
+        .map(|(i, name)| {
+            let mut left_fork = Arc::clone(&forks[i]);
+            let mut right_fork = Arc::clone(&forks[(i + 1) % PHILOSOPHERS.len()]);
+            if correct {
+                if i == forks.len() - 1 {
+                    println!("swapping forks");
+                    std::mem::swap(&mut left_fork, &mut right_fork);
+                }
+            }
+            Philosopher::new(name.to_string(), left_fork, right_fork, tx.clone())
+        })
+        .collect::<Vec<_>>();
+
+    for philosopher in philosophers {
+        thread::spawn(move || {
+            if cfg!(loom) {
+                for _ in 0..10 {
+                    philosopher.eat();
+                    philosopher.think();
+                }
+            } else {
+                for _ in 0..100 {
+                    philosopher.eat();
+                    philosopher.think();
+                }
+            }
+        });
+    }
+
+    drop(tx);
+    #[cfg(loom)]
+    for _ in 0..30 {
+        match rx.recv() {
+            Ok(thought) => println!("{thought}"),
+            Err(e) => {
+                println!("Error: {e}");
+                break;
+            }
+        }
+    }
+
+    #[cfg(not(loom))]
+    for thought in rx {
+        println!("{thought}");
+    }
+
+    println!("All done!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn trigger_deadlock() {
+        use std::sync::mpsc::channel;
+        use std::time::Duration;
+
+        let (done_tx, done_rx) = channel();
+        std::thread::spawn(move || {
+            solution(false);
+            done_tx.send(()).unwrap();
+        });
+        match done_rx.recv_timeout(Duration::from_secs(5)) {
+            Ok(_) => {}
+            Err(e) => panic!("Test timed out, potential deadlock detected: {}", e),
+        }
+    }
+
+    #[test]
+    fn run_correct_solution() {
+        solution(true);
+    }
+
+    #[test]
+    #[should_panic]
+    fn trigger_deadlock_with_loom() {
+        loom::model(|| solution(false));
+    }
+
+    #[test]
+    fn run_correct_solution_with_loom() {
+        loom::model(|| solution(true));
+    }
+}
